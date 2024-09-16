c58ef461c1eabdc3eeb1a8c96b4268d503573d74
Mingbin Yang <Mingbin.Yang@outlook.com>
2024-09-16 13:57:11 -0400

    stairs(slope), floor

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] bool canMove = true;
     [SerializeField] Vector3 moveDir = Vector3.zero;
     public bool isRunning = false;
+    public float gravity = -9.8f;
 
     private void Start()
     {
@@ -21,12 +22,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * y;
+        move.y = characterController.isGrounded ? gravity : move.y;
         isRunning = Input.GetKey(KeyCode.LeftShift) ? true : false;
         characterController.Move(move * (isRunning ? runSpeed : moveSpeed) * Time.deltaTime);
-
     }
 }
