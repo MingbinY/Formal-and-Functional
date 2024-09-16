08a85048a0017143f1cb1605dd74334f4c17f78f
Mingbin Yang <Mingbin.Yang@outlook.com>
2024-09-16 00:23:35 -0400

    FPS controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerManager playerManager;
+    CharacterController characterController;
+    public float moveSpeed = 2f;
+    public float runSpeed = 4f;
+    [SerializeField] bool canMove = true;
+    [SerializeField] Vector3 moveDir = Vector3.zero;
+    public bool isRunning = false;
+
+    private void Start()
+    {
+        playerManager = GetComponent<PlayerManager>();
+        characterController = playerManager.characterController;
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * y;
+        isRunning = Input.GetKey(KeyCode.LeftShift) ? true : false;
+        characterController.Move(move * (isRunning ? runSpeed : moveSpeed) * Time.deltaTime);
+
+    }
+}
